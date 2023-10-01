f7e377280587113c07067b4f796b446f12d71216
avanteijlingen <57176642+avanteijlingen@users.noreply.github.com>
2023-10-02 00:08:46 +0100

    removed tqdm

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pysasa',
-    version='0.0.3',
+    version='0.0.4',
     description='A module for calculating the solvent accessible surface area of molecules',
     url='https://github.com/avanteijlingen/pySASA',
     author='Alexander van Teijlingen',
